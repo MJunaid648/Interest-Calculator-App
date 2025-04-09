a667e971d0c71e9fe62eb47864040bea035396d8
Muhammad Junaid <648mjunaid@gmail.com>
2025-04-09 16:20:25 +0500

    migrated to modules

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,4 @@
 import { Component, computed, inject } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 type Data = {
   year: number | string;
@@ -11,8 +10,6 @@ type Data = {
 };
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
